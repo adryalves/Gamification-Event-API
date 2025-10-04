430b52a5f51c0d6c194819d4353159029960dcd6
adryalves <adryellenalves09@gmail.com>
2025-10-03 22:38:14 -0300

    creating the architecture

---
+++ GamificationEvent.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GamificationEvent.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ GamificationEvent.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GamificationEvent.Core
+{
+    public class Class1
+    {
+
+    }
+}
