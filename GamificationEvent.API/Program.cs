489bbc9f9959129189d490178468c9c02bdf83e8
adryalves <adryellenalves09@gmail.com>
2025-10-07 22:55:02 -0300

    adding ef configs

--- GamificationEvent.API/Program.cs
+++ GamificationEvent.API/Program.cs
@@ -1,8 +1,29 @@
+using GamificationEvent.Infrastructure.Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("MySQLConnection");
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString), // AGORA ISTO VAI FUNCIONAR
+        b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+    );
+});
+
+
 builder.Services.AddControllers();
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
